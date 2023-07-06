1375ad44e52b70ff9fd21ee17eca012b075ea39b
JoseRupallanDiaz <j.rupallan01@ufromail.cl>
2023-07-06 14:28:32 -0400

    accept url-encoded

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import newsRouter from './src/routes/news.router.js';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.use('/auth', userRouter);
 app.use('/news', newsRouter);
